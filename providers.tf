6d2a6cdaf7d19717c0713242e678f98f4ebf9fa9
Jason Hollis <400979+CodeBleu@users.noreply.github.com>
2025-04-24 11:48:28 -0400

    Update to use Apache Cloudstack Provider

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     cloudstack = {
-      source   = "american-cloud/cloudstack"
-      version  = ">= 0.4.1"
+      source   = "cloudstack/cloudstack"
+      version  = "0.5.0"
     }
   }
 }
